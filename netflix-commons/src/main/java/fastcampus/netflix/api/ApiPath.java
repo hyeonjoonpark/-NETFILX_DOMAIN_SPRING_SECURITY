6324b423851cfe4c8b00d31e4287cc7c984f79dd
hyeonjoonpark <pjjoon1379@gmail.com>
2024-11-22 12:26:13 +0900

    FEAT: ApiPath
    API url 클래스 생성

---
+++ netflix-commons/src/main/java/fastcampus/netflix/api/ApiPath.java
@@ -0,0 +1,6 @@
+package fastcampus.netflix.api;
+
+public class ApiPath {
+    public static final String BASE_URL = "/api/v1";
+    public static final String MOVIE_API_URL = BASE_URL + "/movie";
+}
