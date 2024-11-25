4681cf242addcf4763b3b51d9e76d5756c49e965
hyeonjoonpark <pjjoon1379@gmail.com>
2024-11-25 13:43:50 +0900

    Update info
    build.gradle.kts
    application.yml

--- netflix-apps/app-api/build.gradle.kts
+++ netflix-apps/app-api/build.gradle.kts
@@ -1,5 +1,7 @@
 dependencies {
     implementation(project(":netflix-core:core-usecase"))
     implementation(project(":netflix-commons"))
+
     implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
